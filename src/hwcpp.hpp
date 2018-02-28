50a73d423579ada3b04e43cbba9f31975d04d08b
Wouter van Ooijen <wouter@voti.nl>
2018-02-28 20:47:42 +0100

    update

--- src/hwcpp.hpp
+++ src/hwcpp.hpp
@@ -11,12 +11,12 @@
 
 #if defined(ARDUINO_ARCH_AVR)
    #define _HWCPP_TARGET_OK
-   #include "../include/targets/hwcpp-target-arduino-uno.hpp"
+   #include "include/targets/hwcpp-target-arduino-uno.hpp"
 #endif
 
-#elif defined(ARDUINO_ARCH_SAM)
+#if defined(ARDUINO_ARCH_SAM)
    #define _HWCPP_TARGET_OK
-   #include "../lincude/targets/hwcpp-target-arduino-due.hpp"
+   #include "include/targets/hwcpp-target-arduino-due.hpp"
 #endif
 
 #ifndef _HWCPP_TARGET_OK
